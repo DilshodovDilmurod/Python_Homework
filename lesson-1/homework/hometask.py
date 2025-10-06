6f054f78a5b32b8b1e834dd70e06cc81b044f422
ddilshodov1 <126846397+DilshodovDilmurod@users.noreply.github.com>
2025-10-06 12:27:08 +0500

    Create hometask.py

---
+++ lesson-1/homework/hometask.py
@@ -0,0 +1,26 @@
+#1
+var = 21
+perimetr = 4 * var
+area = var **2
+print(f'Perimetri {perimetr}, yuzasi {area} ga teng')
+
+#2
+diam = 23
+PI = 3.14
+len = diam * PI
+print(f'Aylananing uzunligi: {diam * PI}')
+
+#3
+a = 42
+b = 56
+mean = (a + b)/2
+print(mean)
+
+#4
+a = 33
+b = 44
+summ = a + b
+product = a * b
+squarea = a**2
+squareb = b**2
+print(f'sum: {summ}, Product:{product}, squarea :{squarea}, squareb: {squareb}')
