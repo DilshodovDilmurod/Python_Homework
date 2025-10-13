8f3b913a1171fc10d785afd6326dab0a67235bd9
ddilshodov1 <126846397+DilshodovDilmurod@users.noreply.github.com>
2025-10-13 15:59:22 +0500

    Create hometask.py

---
+++ lesson-11/homework/hometask.py
@@ -0,0 +1,17 @@
+PS D:\learning\python homework> 
+PS D:\learning\python homework>  python -m venv venv 
+PS D:\learning\python homework> venv\Scripts\activate
+(venv) PS D:\learning\python homework> pip install numpy 
+Collecting numpy
+  Downloading numpy-2.3.3-cp314-cp314-win_amd64.whl.metadata (60 kB)
+Downloading numpy-2.3.3-cp314-cp314-win_amd64.whl (12.9 MB)
+   ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 12.9/12.9 MB 3.4 MB/s  0:00:03
+Installing collected packages: numpy
+Successfully installed numpy-2.3.3
+(venv) PS D:\learning\python homework> pip list
+Package Version
+------- -------
+numpy   2.3.3
+pip     25.2
+(venv) PS D:\learning\python homework> deactivate
+PS D:\learning\python homework>
